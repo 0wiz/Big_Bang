3999911d3b960b17a68c7a5ea3bcda78c85faf95
Owiz <89930277+0wiz@users.noreply.github.com>
2021-10-17 14:51:02 +0200

    Code version 3.0
    Unfortunetly the Circle function-file wasn't saved and it's unlikely I'll spend time making an accurete recreation.

--- Big_Bang.m
+++ Big_Bang.m
@@ -1,25 +1,44 @@
 % Big Bang - Matlabprojekt i kursen Ingenjorsvetenskap
 
-n = 50; G = 5; % n=NumberOfParticles G=GravityConst p=Particles
-% [ 1=Mass 2=X-Pos 3=Y-Pos 4=X-Vel 5=Y-Vel 6=Radius 7=Num ]
-p = [randi(50,1,n)+150;ones(1,n)*250;ones(1,n)*200;zeros(3,n);1:n];
+function F = Big_Bang(n, count) % n=NrOfParticles
+PosAndVel = zeros(4,1); begin = round(n^(1/3))*7;
 
-% Functional Declarations
-circle = [cos(linspace(0,2*pi,50));sin(linspace(0,2*pi,50))];
-frame = 0; p(6,:) = sqrt(p(1,:)/pi); Circle = @(r,c) r*circle+c;
-A = [0 500 0 400]; F = struct('cdata',[],'colormap',[]);
+% Camera Declarations
+s = [rand(1,300)*4;rand(2,300)*3000-1500];
+F = struct('cdata',[],'colormap',[]); frame = 0; mS = 'MarkerSize';
+figure('Color','white'), set(figure(1),'Position', [300 0 800 800])
 
-% Calculate Acceleration from initial Force
-ini = [randi(50,1,n)+20;pi*randi(200,1,n)/100];
-acce = ini(1,:)./p(1,:).*[cos(ini(2,:));sin(ini(2,:))];
-
-for s=1:25, frame = frame+1; hold on
-    for i = 1:n
-        % Velocitly += Acceleration Position += Velocity
-        p(4:5,i) = p(4:5,i)+acce(:,i); p(2:3,i) = p(2:3,i)+p(4:5,i);
-        c = Circle(p(6,i),p(2:3,i)); plot(c(1,:),c(2,:),'b'), axis(A)
-    end, hold off, F(frame) = getframe(); clf % Capture frame & clear
+for bangCount = 1:count
+    % Particle(p): 1=Mass 2:3=Pos 4:5=Vel 6=Radius
+    p = [randi(50,1,n)+150;zeros(5,n)];
+    p(2:5,:) = repelem(PosAndVel,1,n); p(6,:) = sqrt(p(1,:)/pi);
+    
+    % Calculate Acceleration from initial Force
+    ini = [randi(50,1,n)+20;pi*randi(200,1,n)/100];
+    acc = ini(1,:)./p(1,:).*[cos(ini(2,:));sin(ini(2,:))];
+    
+    for startIn = begin:(-1):0
+        frame = frame+1; C = zeros(3,1); hold on
+        
+        % Draw background stars and adjust zoom
+        for i = 1:size(s,2), plot(s(2,i),s(3,i),'*b',mS,2+s(1,i)); end
+        
+        % Update particles
+        for i = 1:n
+            p(2:3,i) = p(2:3,i)+p(4:5,i); % Pos += Vel
+            p(4:5,i) = p(4:5,i)+acc(:,i); % Vel += Acc
+            Circle(p(6,i),p(2:3,i),'k','k') % Draw particle
+            C(1) = C(1)+p(1,i); % Calculate center of mass
+            C(2:3) = (C(2:3)*C(1)+p(2:3,i)*p(1,i))/C(1);
+        end
+        
+        % Center, zoom & finish up camera before image capture
+        hold off, set(gca,'visible','off')
+        A = [-1 1]*(max(max(abs(p(2:3,:)-C(2:3))))+max(p(6,:))*2);
+        axis([A+C(2) A+C(3)]), F(frame) = getframe(); clf
+    end
+    
+    % Collect particles again and catch changes
+    [p,F] = Gravity(p,s,F); PosAndVel = p(2:5,1);
+    frame = numel(cellfun('isempty',{F.cdata}));
 end
-
-F = Gravity(p,G,A,F,frame);
-movie(F)
--- Gravity.m
+++ Gravity.m
@@ -1,50 +1,43 @@
 % Gravitation - Matlabprojekt i kursen Ingenjorsvetenskap
 
-function F = Gravity(p,G,A,F,frame)
+function [p,F] = Gravity(p,s,F,Circle)
+% Define some variables and derive others from input data
+n = size(p,2); frame = numel(cellfun('isempty',{F.cdata}));
+mS = 'MarkerSize';
 
-% Functional Declarations
-circle = [cos(linspace(0,2*pi,50));sin(linspace(0,2*pi,50))];
-Circle = @(r,c) r*circle+c; endIn = 10; n = size(p,2);
-
-while (endIn>0), frame = frame+1; i = 0; hold on
-    if (n == 1), endIn = endIn-1; end % Adds 10 frames to end of movie
+while (p(6,1)>6.9), frame = frame+1; C = zeros(3,1); hold on
+    % Draw background stars and adjust zoom
+    for i = 1:size(s,2), plot(s(2,i),s(3,i),'*b',mS,2+s(1,i)); end
     
-    while (i < n), i = i+1; % Check for collision between Particles
+    for i = 1:n, rest = [1:i-1 i+1:n];
+        % Update particles
+        dist = p(2:3,rest)-p(2:3,i); % Dists = Positions-Pos
+        dir = atan2(dist(2,:),dist(1,:)); % Directions to p(rest)
+        acc = sum(5*p(1,rest)./sum(dist.^2).*[cos(dir);sin(dir)],2);
+        if (sum(acc.^2)<100), p(4:5,i) = p(4:5,i)+acc;
+        else, p(4:5,i) = p(4:5,i)+.001*acc; end
+        p(2:3,i) = p(2:3,i)+p(4:5,i);
+        
+        % Draw parcticle & Calculate center of mass
+        Circle(p(6,i),p(2:3,i),'k','k')
+        C = [C(1)+p(1,i);(C(2:3)*C(1)+p(2:3,i)*p(1,i))/(C(1)+p(1,i))];
+    end, hold off
+    A = [-1 1]*(max(max(abs(p(2:3,:)-C(2:3))))+max(p(6,:))*2);
+    axis([A+C(2) A+C(3)]), set(gca,'visible','off')
+    F(frame) = getframe(); clf, i = 0;
+    if (n==1), p(6,1) = p(6,1)*.8; end
+    
+    % Check for collision between Particles
+    while (i<n), i = i+1;
         for j = [1:i-1 i+1:n], if (i > n), break, end, ij = [i j];
-            if (sqrt(sum((p(2:3,j)-p(2:3,i)).^2)) <= max(p(6,ij)))
-                [~,big] = max(p(1,ij)); big = ij(big); % Heaviest
+            if (sum((p(2:3,j)-p(2:3,i)).^2) < max(p(6,ij))^2)
+                [~,big] = max(p(1,ij)); big = ij(big); % Biggest
                 Mass = sum(p(1,ij)); % New mass is sum of masses
-                Pos = p(2:3,big); % Position is from heaviest
-                Vel = sum(p(4:5,ij).*p(1,ij),2)/sum(p(1,ij));
-                Rad = sqrt(sum(p(1,ij))/pi); % Radius from new mass
-                p(:,min(ij)) = [Mass;Pos;Vel;Rad;p(7,big)];
+                Vel = sum(p(4:5,ij).*p(1,ij),2)/Mass; % Momentum
+                p(:,min(ij)) = [Mass;p(2:3,big);Vel;sqrt(Mass/pi)];
                 p = p(:,[1:max(ij)-1 max(ij)+1:n]); n = n-1; i = i-1;
                 break
             end
         end
     end
-    for i = 1:n, others = [1:i-1 i+1:n];
-        % Expand screenspace if particle comes out of bounds
-        if (p(2,i)<A(1) || p(2,i)>A(2) || p(3,i)<A(3) || p(3,i)>A(4))
-            A = A + [-300 300 -200 200];
-        end
-        
-        % Plot particle as circle
-        c = Circle(p(6,i),p(2:3,i)); plot(c(1,:),c(2,:),'b')
-        axis(A)%, text(p(2,i)-5,p(3,i),int2str(p(7,i)))
-        
-        % Distances = Positions - Position
-        dist = p(2:3,others)-p(2:3,i);
-        [mDist,mIdx] = min(sum(dist.^2));
-        if (mDist > sum(p(6,[i others(mIdx)]))^2)
-            dire = atan2(dist(2,:),dist(1,:));
-            % Acceleration = G * Masses / Distances^2
-            acce = G*p(1,others)./sum(dist.^2);
-            % Velocitly += Acceleration
-            p(4:5,i) = p(4:5,i)+sum(acce.*[cos(dire);sin(dire)],2);
-        end
-        % Position += Velocity
-        p(2:3,i) = p(2:3,i)+p(4:5,i);
-    end
-    hold off, F(frame) = getframe(); clf % Capture frame & clear
 end
